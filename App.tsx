b9aab432be91076d4375fb74d701e521ce3218ac
Luan Tonin Galvan <luan.galvan@luizalabs.com>
2023-01-01 22:23:53 -0300

    feat: reprodução da rádio

--- App.tsx
+++ App.tsx
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Buscar status
+// http://shoutcast.tvcbrasilia.tv:8000/stats?sid=1&json=1
 
-export default function App() {
+// Reproduzir música
+// http://shoutcast.tvcbrasilia.tv:8000/;?type=http&nocache=36557
+
+import React, { useEffect, useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from "expo-av";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const ICON_PLAY_BUTTON = "play-circle-outline";
+const ICON_PAUSE_BUTTON = "pause-circle-outline";
+
+const Settings = () => {
+  Audio.setAudioModeAsync({
+    allowsRecordingIOS: false,
+    staysActiveInBackground: true,
+    interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+    playsInSilentModeIOS: true,
+    shouldDuckAndroid: true,
+    interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+    playThroughEarpieceAndroid: false,
+  });
+};
+
+const soundObject = new Audio.Sound();
+
+const URL_STREAMING =
+  "http://shoutcast.tvcbrasilia.tv:8000/;?type=http&nocache=36557";
+
+export default () => {
+  useEffect(() => {
+    Settings(), initSoundObject();
+  }, []);
+  const [playing, setPlaying] = useState(false);
+
+  const stateAudio = {
+    shouldPlay: false,
+    shouldCorrectPitch: true,
+    volume: 1.0,
+    isMuted: false,
+  };
+
+  const initSoundObject = () => {
+    soundObject.loadAsync({ uri: URL_STREAMING }, stateAudio);
+  };
+
+  const playAudio = async () => {
+    try {
+      if (playing) {
+        await soundObject.stopAsync();
+        setPlaying(false);
+      } else {
+        await soundObject.playAsync();
+        setPlaying(true);
+      }
+    } catch (error) {
+      console.log("Error when playMusic", error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableOpacity style={styles.iconTouchable} onPress={() => playAudio()}>
+      <MaterialIcons
+        name={playing ? ICON_PAUSE_BUTTON : ICON_PLAY_BUTTON}
+        size={80}
+        color="blue"
+      />
+    </TouchableOpacity>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+  },
+  iconTouchable: {
+    backgroundColor: "#000",
   },
 });
